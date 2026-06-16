70b6ea1d7b1a3d169a8ac70e17d2b19cf7d5b953
Wakanda Gamasis <129169589+Wakanda-Gamsis@users.noreply.github.com>
2026-06-16 22:51:51 +0530

    Revert "Shift experimental EyeControl to test branch"
    This reverts commit b64f5158b007dbfdc25be8773a9512b6a05f7805.

---
+++ EyeControl/__init__.py
@@ -0,0 +1,12 @@
+"""Optional eye-controlled mouse tracker integration for Fiona."""
+
+from __future__ import annotations
+
+from .Eye_Controlled_Mouse_Tracker import DEFAULT_CAMERA_URL, EyeTrackerConfig, dependency_status, run_eye_tracker
+
+__all__ = [
+    "DEFAULT_CAMERA_URL",
+    "EyeTrackerConfig",
+    "dependency_status",
+    "run_eye_tracker",
+]
